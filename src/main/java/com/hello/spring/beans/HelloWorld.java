f8a97888c048e79046ae8b0921f889c2291becde
lvxiaogua@foxmail.com <jakefy123>
2016-10-18 16:55:28 +0800

    init

---
+++ src/main/java/com/hello/spring/beans/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.hello.spring.beans;
+
+/**
+ * 2016-10-18.
+ */
+public class HelloWorld {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void hello() {
+        System.out.println("Hello " + name);
+    }
+
+}
---
+++ src/main/java/com/hello/spring/beans/Main.java
@@ -0,0 +1,15 @@
+package com.hello.spring.beans;
+
+/**
+ * 2016-10-18.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setName("Jone");
+
+        helloWorld.hello();
+    }
+
+}
